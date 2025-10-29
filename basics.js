b60197fc39220d5a30ed84e8052aac4d6deba80e
sandiliviwe04 <sandi.liviwe04@gmail.com>
2025-10-29 12:42:57 +0200

    Initial commit with basics.js

---
+++ basics.js
@@ -0,0 +1,7 @@
+const firstName = "Liviwe";
+const lastName = "Sandi";
+let age = 21;
+const fullName = firstName + " " + lastName;
+console.log('Hello, my name is ${fullName} and I am ${age} years old.');
+age = 26;
+console.log('Hello, my name is ${fullName} and I am ${age} years old.');
